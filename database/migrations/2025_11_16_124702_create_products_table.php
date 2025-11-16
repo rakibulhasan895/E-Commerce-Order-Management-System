dc4327cf2c38b6e64d7d39d26c9efd232f9dafed
rakibulhasan895 <rakibulhasan.rh895@gmail.com>
2025-11-16 18:53:48 +0600

    database

---
+++ database/migrations/2025_11_16_124702_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('vendor_id')->constrained('users');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->longText('description')->nullable();
+            $table->decimal('base_price', 10, 2);
+            $table->boolean('status')->default(1);
+            $table->integer('low_stock_threshold')->default(5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
